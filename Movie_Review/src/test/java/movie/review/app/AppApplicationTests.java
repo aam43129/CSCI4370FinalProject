65229189ad34d58deba7669c66ef2fe9a61fbc13
Jayden Burrows <jaydenborrows@gmail.com>
2026-04-21 00:20:57 -0400

    Added a starter Maven project based on start.spring.io and previous project

---
+++ Movie_Review/src/test/java/movie/review/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.review.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSCI4370FinalProject
+
+For future reference: 
+.\mvnw.cmd spring-boot:run   
+http://localhost:8080/
