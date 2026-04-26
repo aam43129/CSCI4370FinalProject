20795240c33bf47a950025a71b8abb1a55f40fd5
Jayden Burrows <jaydenborrows@gmail.com>
2026-04-26 18:54:33 -0400

    Added some controller methods and fixed the ui a little.

--- Movie_Review/src/main/java/movie/review/app/controllers/MovieController.java
+++ Movie_Review/src/main/java/movie/review/app/controllers/MovieController.java
@@ -1,5 +1,8 @@
 package movie.review.app.controllers;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +43,50 @@ public class MovieController {
 
         String errorMessage = error;
         mv.addObject("errorMessage", errorMessage);
-        
+
         return mv;
     }
+
+    @GetMapping("/{movieId}/list/{isAdd}")
+    public String addOrRemoveList(@PathVariable("movieId") String movieId,
+            @PathVariable("isAdd") Boolean isAdd) {
+        String currentUserId = userService.getLoggedInUser().getUserId();
+
+        // boolean isSuccess = isAdd
+        //         ? movieService.addToList(currentUserId, movieId)
+        //         : movieService.removeFromList(currentUserId, movieId);
+        boolean isSuccess = isAdd;
+
+        if (isSuccess) {
+            return "redirect:/movies/" + movieId;
+        }
+        String message = URLEncoder.encode("Failed to (un)like the post. Please try again.",
+                StandardCharsets.UTF_8);
+        return "redirect:/post/" + movieId + "?error=" + message;
+    }
+
+    @PostMapping("/{movieId}/create-review")
+    public String postReview(@PathVariable("movieId") String movieId,
+            @RequestParam(name = "rating") String rating,
+            @RequestParam(name = "content") String content) {
+        System.out.println("The user is attempting add a comment:");
+        System.out.println("\tmovieId: " + movieId);
+        System.out.println("\trating: " + rating);
+        System.out.println("\tcontent: " + content);
+        String userId = userService.getLoggedInUser().getUserId();
+
+        // boolean postSuccessful = movieService.postReview(movieId, userId, rating, content);
+        boolean postSuccessful = true;
+        // Redirect the user if the comment adding is a success.
+        if (postSuccessful) {
+            return "redirect:/movies/" + movieId;
+        }
+
+        // Redirect the user with an error message if there was an error.
+        String message = URLEncoder.encode("Failed to post the comment. Please try again.",
+                StandardCharsets.UTF_8);
+        return "redirect:/movies/" + movieId + "?error=" + message;
+    }
+
 }
+
